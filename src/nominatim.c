bb4f663aebd8143f4562e28935db5da6bc1ef778
Antonin FREY <antonin.frey@telecomnancy.eu>
2023-05-10 17:21:21 +0200

    [feat] base nominatim defs

---
+++ src/nominatim.c
@@ -0,0 +1,33 @@
+#include "nominatim.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "coordinates.h"
+
+/**
+ * @brief Create a List_t object
+ *
+ * @return List_t*
+ */
+
+char *fetch_api(char *query)
+{
+    // ...
+    // return responseText
+}
+
+Nominatim_t *parse_nominatim(char *response)
+{
+    // ...
+    // return nomin
+}
+
+Nominatim_t *get_nominatim(char *query)
+{
+    // ...
+    // return nomin
+}
+
---
+++ src/nominatim.h
@@ -0,0 +1,33 @@
+#ifndef NOMINATIM_H
+#define NOMINATIM_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "coordinates.h"
+
+struct _Nominatim_t
+{
+    char *name;
+    Coordinates_t *coord;
+};
+/**
+ * @typedef Nominatim_t
+ * @brief Nominatim type
+ *
+ * @var Nominatim_t::name
+ * Name of the nominatim
+ * @var Nominatim_t::coord
+ * Coordinates of the nominatim
+ */
+typedef struct _Nominatim_t Nominatim_t;
+
+char *fetch_api(char *query);
+
+Nominatim_t *parse_nominatim(char *response);
+
+Nominatim_t *get_nominatim(char *query);
+
+#endif /* NOMINATIM_H */
