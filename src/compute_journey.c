4ab933e58dcb574da1ab5ac7889679100969923b
Antonin FREY <antonin.frey@telecomnancy.eu>
2023-05-12 11:24:31 +0200

    [refactor] compute_journey raw method

--- src/compute_journey.c
+++ src/compute_journey.c
@@ -1,7 +1,49 @@
+#include "compute_journey.h"
 #include "a_star.h"
 #include "nominatim.h"
 #include <stdio.h>
 
+/**
+ * @brief Compute the journey between two points
+ * 
+ * @param startNomin Nominatim_t* Start point
+ * @param endNomin Nominatim_t* End point
+ * @param vehicleName char* Name of the vehicle
+ * @return Journey_output_t Journey output
+*/
+Journey_output_t compute_journey(Nominatim_t *startNomin, Nominatim_t *endNomin, char *vehicleName)
+{
+    Table_t *table = load_stations("../data/raw/consolidation-etalab-schema-irve-statique-v-2.2.0-20230415.csv");
+    Coordinates_t *startCoordinates = malloc(sizeof(Coordinates_t)), *endCoordinates = malloc(sizeof(Coordinates_t));
+    char *startKey = malloc(6), *endKey = malloc(4);
+    Vehicle_t vehicle = vehicle_find_by_name(vehicleName);
+
+    startCoordinates->latitude = startNomin->coord->latitude;
+    startCoordinates->longitude = startNomin->coord->longitude;
+    endCoordinates->latitude = endNomin->coord->latitude;
+    endCoordinates->longitude = endNomin->coord->longitude;
+
+    Station_t *start = station_create(startNomin->name, startCoordinates, 0, 0, 0);
+    Station_t *end = station_create(endNomin->name, endCoordinates, 0, 0, 0);
+    strcpy(startKey, "start");
+    strcpy(endKey, "end");
+    table_add(table, startKey, start);
+    table_add(table, endKey, end);
+
+    List_t *journey = a_star_list(table, "start", "end", &vehicle);
+
+    Journey_output_t output = {table, journey};
+
+    return output;
+}
+
+/**
+ * @brief Compute the journey from the command line
+ * 
+ * @param argc int argument count
+ * @param argv char* argument vector
+ * @return int 0 if success, 1 if error
+*/
 int main(int argc, char *argv[])
 {
     if (argc != 4)
@@ -10,31 +52,27 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    Table_t *table = load_stations("../data/raw/consolidation-etalab-schema-irve-statique-v-2.2.0-20230415.csv");
-    Coordinates_t *start_coordinates = malloc(sizeof(Coordinates_t)), *end_coordinates = malloc(sizeof(Coordinates_t));
-    char *start_key = malloc(6), *end_key = malloc(4);
-    Vehicle_t vehicle = vehicle_find_by_name(argv[3]);
+    Nominatim_t *startNomin = nominatim_fetch(argv[1]);
+    Nominatim_t *endNomin = nominatim_fetch(argv[2]);
 
-    Nominatim_t *start_nomin = nominatim_fetch(argv[1]);
-    start_coordinates->latitude = start_nomin->coord->latitude;
-    start_coordinates->longitude = start_nomin->coord->longitude;
-    Nominatim_t *end_nomin = nominatim_fetch(argv[2]);
-    end_coordinates->latitude = end_nomin->coord->latitude;
-    end_coordinates->longitude = end_nomin->coord->longitude;
-    Station_t *start = station_create(start_nomin->name, start_coordinates, 0, 0, 0);
-    Station_t *end = station_create(end_nomin->name, end_coordinates, 0, 0, 0);
-    strcpy(start_key, "start");
-    strcpy(end_key, "end");
-    table_add(table, start_key, start);
-    table_add(table, end_key, end);
+    if (startNomin == NULL || endNomin == NULL)
+    {
+        fprintf(stderr, "\033[31m>> Error: \033[0mInvalid location\n");
+        nominatim_destroy(startNomin);
+        nominatim_destroy(endNomin);
+        return 1;
+    }
 
-    List_t *journey = a_star_list(table, "start", "end", &vehicle);
+    Journey_output_t output = compute_journey(startNomin, endNomin, argv[3]);
+    Table_t *table = output.table;
+    List_t *journey = output.journey;
+
     print_a_star(table, journey);
 
     table_destroy(table);
     list_destroy(journey);
-    nominatim_destroy(start_nomin);
-    nominatim_destroy(end_nomin);
+    nominatim_destroy(startNomin);
+    nominatim_destroy(endNomin);
 
     return 0;
 }
---
+++ src/compute_journey.h
@@ -0,0 +1,24 @@
+#include "a_star.h"
+#include "nominatim.h"
+#include <stdio.h>
+
+
+struct _Journey_output_t {
+    Table_t *table;
+    List_t *journey;
+};
+/**
+ * @typedef Journey_output_t
+ * @brief Compute journey output type
+ *
+ * @var Journey_output_t::table
+ * Table of the journey graph
+ * @var Journey_output_t::journey
+ * List of steps of the journey
+ */
+typedef struct _Journey_output_t Journey_output_t;
+
+Journey_output_t compute_journey(Nominatim_t *startNomin, Nominatim_t *endNomin, char *vehicleName);
+
+int main(int argc, char *argv[]);
+
