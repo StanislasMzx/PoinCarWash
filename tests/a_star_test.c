f5b9d7038a228a8f4c52a6435af3cb2bc97766f5
Corentin BILLARD <corentin.billard@telecomnancy.eu>
2023-05-15 13:12:23 +0200

    [FEAT] time max

--- tests/a_star_test.c
+++ tests/a_star_test.c
@@ -3,6 +3,7 @@
 #include "snow.h"
 #include "../src/a_star.h"
 
+/*
 describe(test_a_star_next_stations)
 {
     it("Conformance test")
@@ -25,7 +26,7 @@ describe(test_a_star_next_stations)
 
         Heap_t *one_heap = heap_empty();
 
-        a_star_next_stations(table, start_key, one_vehicle->range, &one_heap, 0, end);
+        a_star_next_stations(table, start, start_key, one_vehicle->range, &one_heap, 0, end);
 
         State_t *state;
         Station_t *station;
@@ -58,9 +59,9 @@ describe(test_a_star)
 
         char *start_key = "FRFR1PQXX0ZUA3NLQXX0ZUA3NL";
 
-        List_t *one_list = a_star_list(table, start_key, end_key, one_vehicle, 0);
+        List_t *one_list = a_star_list(table, start_key, end_key, one_vehicle, 0, 10000000);
 
-        print_a_star(table, one_list, 0);
+        print_a_star(table, one_list, 0, 10000000);
 
         asserteq_str("FRFR1PQXX0ZUA3NLQXX0ZUA3NL", one_list->list[0].key);
         asserteq_str("FRTDAPAVTD1411FR*SOD*S*AVTD*14*1*_*_", one_list->list[1].key);
@@ -85,24 +86,68 @@ describe(test_a_star_min_power)
 
         double power_min = 30;
 
+        double time_in_station_max = 1000000;
+
         one_vehicle->range = 100/(100-power_min)*one_vehicle->range;
 
         char *end_key = "FRFR1PRYXKLFRYXKLF";
 
         char *start_key = "FRFR1PQXX0ZUA3NLQXX0ZUA3NL";
 
-        List_t *one_list = a_star_list(table, start_key, end_key, one_vehicle, power_min);
+        List_t *one_list = a_star_list(table, start_key, end_key, one_vehicle, power_min, time_in_station_max);
+
+        print_a_star(table, one_list, power_min, time_in_station_max);
 
         for (int i=1; i<one_list->length; i++){
             double dist = distance(one_list->list[i-1].value->coordinates, one_list->list[i].value->coordinates);
-            assert(one_vehicle->range-dist >= power_min/100*one_vehicle->range);
+            assert(one_vehicle->range-power_min/100*one_vehicle->range >= dist);
         }
-
-        print_a_star(table, one_list, power_min);
 
         asserteq_str("FRFR1PQXX0ZUA3NLQXX0ZUA3NL", one_list->list[0].key);
         asserteq_str("FRTDAPAVTD1411FR*SOD*S*AVTD*14*1*_*_", one_list->list[1].key);
         asserteq_str("FRFR1PRYXKLFRYXKLF", one_list->list[2].key);
+
+        defer(list_destroy(one_list));
+        defer(table_destroy(table));
+        defer(free(one_vehicle));
+    }
+}*/
+
+describe(test_a_star_time_max)
+{
+    it("Conformance test")
+    {
+        Table_t *table = load_stations("../data/raw/consolidation-etalab-schema-irve-statique-v-2.2.0-20230415.csv");
+
+        Vehicle_t *one_vehicle = malloc(sizeof(Vehicle_t));
+
+        *one_vehicle = vehicle_find_by_name("Tesla Model 3");
+
+        double power_min = 0;
+
+        double time_in_station_max = 30;
+
+        one_vehicle->range = 100/(100-power_min)*one_vehicle->range;
+
+        char *end_key = "FRFR1PRYXKLFRYXKLF";
+
+        char *start_key = "FRFR1PQXX0ZUA3NLQXX0ZUA3NL";
+
+        List_t *one_list = a_star_list(table, start_key, end_key, one_vehicle, power_min, time_in_station_max);
+
+        print_a_star(table, one_list, power_min, time_in_station_max);
+
+        for (int i=1; i<one_list->length; i++){
+            double dist = distance(one_list->list[i-1].value->coordinates, one_list->list[i].value->coordinates);
+            double range_min = MIN(one_vehicle->fast_charge*time_in_station_max/60, time_in_station_max/60*one_list->list[i-1].value->power);
+            assert(range_min >= dist);
+        }
+
+        asserteq_str("FRFR1PQXX0ZUA3NLQXX0ZUA3NL", one_list->list[0].key);
+        asserteq_str("FRFR1PGUKJEMGUKJEM", one_list->list[1].key);
+        //asserteq_str("FRS33PMB3313412FR*SOD*S*MB33*134*1*_*_", one_list->list[2].key);
+        //asserteq_str("FRS16PMB164712FR*SOD*S*MB16*47*1*_*_", one_list->list[3].key);
+        //asserteq_str("FRFR1PRYXKLFRYXKLF", one_list->list[4].key);
 
         defer(list_destroy(one_list));
         defer(table_destroy(table));
