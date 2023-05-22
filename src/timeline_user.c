bf2495a724df0672efe5898a2dd5ac9797e8db59
Antonin FREY <antonin.frey@telecomnancy.eu>
2023-05-22 19:36:32 +0200

    [fix] use travel_time function

--- src/timeline_user.c
+++ src/timeline_user.c
@@ -207,8 +207,9 @@ int userLocation(Timeline_user_t *one_timeline, int one_tick, Table_t *table){
         // on est parti d'une station
         Station_t *old_station = table_get(table, one_state->station);
         Station_t *new_station = one_timeline->trip->list[one_state->stepTrip].value;
-        double dist = distance(old_station->coordinates, new_station->coordinates);
-        int tick_arrived = (int)floor(dist/100*3.6); // celery = 100km/h
+        int travelTicks = (int)ceil(travel_time(old_station, new_station) / 10.0);
+        // TODO: check behavior
+        int tick_arrived = one_state->tick + travelTicks;
         assert(tick_arrived <= one_tick);
         if (tick_arrived == one_tick){
             return new_station->id;
