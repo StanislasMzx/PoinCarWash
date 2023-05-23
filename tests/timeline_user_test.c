863cb4c5563ee4fb27e167a6cc16dfb1b0039634
Corentin BILLARD <corentin.billard@telecomnancy.eu>
2023-05-23 13:14:14 +0200

    [FIX] travel ticks

--- tests/timeline_user_test.c
+++ tests/timeline_user_test.c
@@ -8,8 +8,8 @@ describe(test_initializeTimelineUser)
 {
     it("Conformance test")
     {
-        Table_t *table = load_stations("../data/raw/consolidation-etalab-schema-irve-statique-v-2.2.0-20230415.csv");
-        Timeline_all_users_t *global_timeline = initializeTimelineUser(table, "../data/input/network1.csv");
+        Table_t *table = load_stations(STATION_TABLE_PATH);
+        Timeline_all_users_t *global_timeline = initializeTimelineUser(table, NETWORK_PATH);
         asserteq_int(global_timeline->lastTick, 2);
         asserteq_int(global_timeline->userNumber, 3);
         asserteq_int(global_timeline->listTimeline[0]->state->tick, 2);
@@ -21,9 +21,27 @@ describe(test_initializeTimelineUser)
         asserteq_int(global_timeline->listTimeline[2]->state->tick, 1);
         asserteq_str(global_timeline->listTimeline[2]->trip->list->value->name, "Paris, Île-de-France, France métropolitaine, France");
         asserteq_str(global_timeline->listTimeline[2]->vehicle->name, "Renault Zoe ZE50 R110");
-        timelineUserDestroyAll(&global_timeline);
-        table_destroy(table);
+        
+        
+        defer(timelineUserDestroyAll(&global_timeline));
+        defer(table_destroy(table));
     }
 }
 
+
+describe(test_user_localition){
+    it("Conformance test"){
+        Table_t *table = load_stations(STATION_TABLE_PATH);
+        Timeline_all_users_t *global_timeline = initializeTimelineUser(table, NETWORK_PATH);
+
+        for (int i=2;i<15;i++){
+            printf("\n%d\n", userLocation(global_timeline->listTimeline[0], i, table));
+        }
+
+        
+
+        defer(timelineUserDestroyAll(&global_timeline));
+        defer(table_destroy(table));
+    }
+}
 snow_main();
