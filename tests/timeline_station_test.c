7ccbaff4b6eaa186472bf5ade5fe4d46f4cb87e8
Corentin BILLARD <corentin.billard@telecomnancy.eu>
2023-05-24 13:34:38 +0200

    [FIX] userLocalization

--- tests/timeline_station_test.c
+++ tests/timeline_station_test.c
@@ -11,11 +11,19 @@ describe(test_initializeTimelineStation)
     Timeline_all_users_t *one_all_user = initializeTimelineUser(table, NETWORK_PATH);
     Timeline_all_stations_t *one_all_station = initializeTimelineAllStation(one_all_user, table);
 
+    asserteq_int(one_all_station->nbStations, 9);
     for (int i=0; i<one_all_station->nbStations; i++){
         asserteq_int(one_all_station->listTimeline[i]->stateValue->tick, 0);
-        assert(one_all_station->listTimeline[i]->next == NULL);
-        asserteq_int(one_all_user->listTimeline[i]->state->idStation, i);
+        assert(one_all_station->listTimeline[i]->next == NULL);   
     }
+
+    assert(table_get(table, "FRFR1PRATIESIM0E")->id != -1);
+    assert(table_get(table, "FRM38P38185022")->id != -1);
+    assert(table_get(table, "FREBNPZFXETGZFXETG")->id != -1);
+    assert(table_get(table, "FRFR1PAJAG0W3H7HAJAG0W3H7H")->id != -1);
+    assert(table_get(table, "FRFR1PWD96MMJNF3WD96MMJNF3")->id != -1);
+    assert(table_get(table, "FRS89PL4SV657FSZL4SV657FSZ")->id != -1);
+
 
     defer(table_destroy(table));
     defer(destroyTimelineAllStations(one_all_station));
